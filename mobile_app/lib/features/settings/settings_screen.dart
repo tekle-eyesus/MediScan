7b1daaef4e8def4e212b1faa96fca4873752fac3
tekle-eyesus <tekleeyesus21@gmail.com>
2026-01-23 11:27:53 -0700

    feat: enhance SettingsScreen with styled AppBar and gradient background

--- mobile_app/lib/features/settings/settings_screen.dart
+++ mobile_app/lib/features/settings/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -6,6 +7,22 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: GoogleFonts.poppins(),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF438EA5), Color(0xFF4DA49C)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        foregroundColor: Colors.white,
+      ),
       body: Center(
         child: Text(
           'Settings Screen',
